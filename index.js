30bec6df8d14ae0353787da4d153c2d5246557d6
Hadrien BRUNAUD <hadrien.brunaud@gmail.com>
2016-03-25 13:14:59 +0100

    Speed improvement
    I left only sites with at least 100k visits/day
    http://stackexchange.com/sites?view=list#traffic

--- index.js
+++ index.js
@@ -47,98 +47,98 @@ module.exports = (pluginContext) => {
             title:"Database Administrators",
             slug:"dba",
             url:"http://dba.stackexchange.com/"
-        },{
-            title:"Seasoned Advice",
-            slug:"cooking",
-            url:"http://cooking.stackexchange.com/"
-        },{
-            title:"Home Improvement",
-            slug:"diy",
-            url:"http://diy.stackexchange.com/"
-        },{
-            title:"TeX - LaTeX",
-            slug:"tex",
-            url:"http://tex.stackexchange.com/"
-        },{
-            title:"Electrical Engineering",
-            slug:"electronics",
-            url:"http://electronics.stackexchange.com/"
-        },{
-            title:"Cross Validated",
-            slug:"stats",
-            url:"http://stats.stackexchange.com/"
-        },{
-            title:"Programmers",
-            slug:"programmers",
-            url:"http://programmers.stackexchange.com/"
-        },{
-            title:"Graphic Design",
-            slug:"graphicdesign",
-            url:"http://graphicdesign.stackexchange.com/"
-        },{
-            title:"Physics",
-            slug:"physics",
-            url:"http://physics.stackexchange.com/"
-        },{
-            title:"Information Security",
-            slug:"security",
-            url:"http://security.stackexchange.com/"
-        },{
-            title:"Science Fiction & Fantasy",
-            slug:"scifi",
-            url:"http://scifi.stackexchange.com/"
-        },{
-            title:"SharePoint",
-            slug:"sharepoint",
-            url:"http://sharepoint.stackexchange.com/"
-        },{
-            title:"Stack Overflow на русском",
-            slug:"ru",
-            url:"http://ru.stackoverflow.com/"
-        },{
-            title:"Geographic Information Systems",
-            slug:"gis",
-            url:"http://gis.stackexchange.com/"
-        },{
-            title:"Code Review",
-            slug:"codereview",
-            url:"http://codereview.stackexchange.com/"
-        },{
-            title:"Web Applications",
-            slug:"webapps",
-            url:"http://webapps.stackexchange.com/"
-        },{
-            title:"English Language Learners",
-            slug:"ell",
-            url:"http://ell.stackexchange.com/"
-        },{
-            title:"WordPress Development",
-            slug:"wordpress",
-            url:"http://wordpress.stackexchange.com/"
-        },{
-            title:"Motor Vehicle Maintenance & Repair",
-            slug:"mechanics",
-            url:"http://mechanics.stackexchange.com/"
-        },{
-            title:"Magento",
-            slug:"magento",
-            url:"http://magento.stackexchange.com/"
-        },{
-            title:"Travel",
-            slug:"travel",
-            url:"http://travel.stackexchange.com/"
-        },{
-            title:"Raspberry Pi",
-            slug:"raspberrypi",
-            url:"http://raspberrypi.stackexchange.com/"
-        },{
-            title:"The Workplace",
-            slug:"workplace",
-            url:"http://workplace.stackexchange.com/"
-        },{
-            title:"Movies & TV",
-            slug:"movies",
-            url:"http://movies.stackexchange.com/"
+        // },{
+        //     title:"Seasoned Advice",
+        //     slug:"cooking",
+        //     url:"http://cooking.stackexchange.com/"
+        // },{
+        //     title:"Home Improvement",
+        //     slug:"diy",
+        //     url:"http://diy.stackexchange.com/"
+        // },{
+        //     title:"TeX - LaTeX",
+        //     slug:"tex",
+        //     url:"http://tex.stackexchange.com/"
+        // },{
+        //     title:"Electrical Engineering",
+        //     slug:"electronics",
+        //     url:"http://electronics.stackexchange.com/"
+        // },{
+        //     title:"Cross Validated",
+        //     slug:"stats",
+        //     url:"http://stats.stackexchange.com/"
+        // },{
+        //     title:"Programmers",
+        //     slug:"programmers",
+        //     url:"http://programmers.stackexchange.com/"
+        // },{
+        //     title:"Graphic Design",
+        //     slug:"graphicdesign",
+        //     url:"http://graphicdesign.stackexchange.com/"
+        // },{
+        //     title:"Physics",
+        //     slug:"physics",
+        //     url:"http://physics.stackexchange.com/"
+        // },{
+        //     title:"Information Security",
+        //     slug:"security",
+        //     url:"http://security.stackexchange.com/"
+        // },{
+        //     title:"Science Fiction & Fantasy",
+        //     slug:"scifi",
+        //     url:"http://scifi.stackexchange.com/"
+        // },{
+        //     title:"SharePoint",
+        //     slug:"sharepoint",
+        //     url:"http://sharepoint.stackexchange.com/"
+        // },{
+        //     title:"Stack Overflow на русском",
+        //     slug:"ru",
+        //     url:"http://ru.stackoverflow.com/"
+        // },{
+        //     title:"Geographic Information Systems",
+        //     slug:"gis",
+        //     url:"http://gis.stackexchange.com/"
+        // },{
+        //     title:"Code Review",
+        //     slug:"codereview",
+        //     url:"http://codereview.stackexchange.com/"
+        // },{
+        //     title:"Web Applications",
+        //     slug:"webapps",
+        //     url:"http://webapps.stackexchange.com/"
+        // },{
+        //     title:"English Language Learners",
+        //     slug:"ell",
+        //     url:"http://ell.stackexchange.com/"
+        // },{
+        //     title:"WordPress Development",
+        //     slug:"wordpress",
+        //     url:"http://wordpress.stackexchange.com/"
+        // },{
+        //     title:"Motor Vehicle Maintenance & Repair",
+        //     slug:"mechanics",
+        //     url:"http://mechanics.stackexchange.com/"
+        // },{
+        //     title:"Magento",
+        //     slug:"magento",
+        //     url:"http://magento.stackexchange.com/"
+        // },{
+        //     title:"Travel",
+        //     slug:"travel",
+        //     url:"http://travel.stackexchange.com/"
+        // },{
+        //     title:"Raspberry Pi",
+        //     slug:"raspberrypi",
+        //     url:"http://raspberrypi.stackexchange.com/"
+        // },{
+        //     title:"The Workplace",
+        //     slug:"workplace",
+        //     url:"http://workplace.stackexchange.com/"
+        // },{
+        //     title:"Movies & TV",
+        //     slug:"movies",
+        //     url:"http://movies.stackexchange.com/"
         },
     ]
 
@@ -151,7 +151,7 @@ module.exports = (pluginContext) => {
             res.add({
                 id: query_trim,
                 payload: site.slug,
-                title: 'Search on ' + site.title + ' : ' + query_trim,
+                title: 'Search ' + query_trim,
                 // icon: '/img/' + site.slug + '.png',
                 desc: 'Search on ' + site.title
             });
